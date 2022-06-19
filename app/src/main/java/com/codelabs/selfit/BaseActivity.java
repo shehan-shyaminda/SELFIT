4efb5533612843852efb1ec8e2d3513d2476cae4
STEPHEN INNOVATIONS <stepheninnovations@192.168.1.2>
2022-06-20 00:31:31 +0530

    Initial Commit

---
+++ app/src/main/java/com/codelabs/selfit/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.codelabs.selfit;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
